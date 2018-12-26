543dca2c3bcab320f66112e1de42c3cdab06697b
lvtdesarrollo <lvtdesarrollo@gmail.com>
2018-12-26 06:47:20 -0500

    update

---
+++ seguimiento_lista_1.php
@@ -0,0 +1,135 @@
+<?php include('header.php'); ?>
+<div class="breadcrumbs">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    <div class="page-title">
+                        <h1>INABIF</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="page-header float-right">
+                    <div class="page-title">
+                        <ol class="breadcrumb text-right">
+                            <li class="active"></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content mt-3">
+        <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong>Lista de Seguimiento</strong>
+                            <h6>Período Noviembre 2018</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="row form-group">
+                                <div class="col-md-1 col-sm-6"><label for="select" class=" form-control-label">Grupo:</label></div>
+                                <div class="col-md-3  col-sm-6">
+                                    <select name="select" id="select" class="form-control">
+                                        <option value="Seleccionar">Seleccionar</option>
+                                        <option value="1">Centro 1</option>
+                                        <option value="2">Grupo 2</option>
+                                        <option value="3">Grupo 3</option>
+                                        <option value="1">Grupo 4</option>
+                                        <option value="2">Grupo 5</option>
+                                        <option value="3">Grupo 6</option>
+                                    </select>
+                                </div>
+                                <div class="col-md-1 col-sm-6"><label for="select" class=" form-control-label">Residente:</label></div>
+                                <div class="col-md-4  col-sm-6">
+                                <input type="text" value="" class="form-control" placeholder="Apellido Cod">
+                                </div>
+                            </div>
+
+                            <div class="table-responsive">
+                                <table class="table">
+                                <thead class="thead-dark text-center">
+                                    <tr>
+                                    
+                                        <th scope="col">Centro</th>
+                                        <th scope="col">Completo</th>
+                                        <th scope="col">Fecha Cierre</th>
+                                        <th scope="col">Cerrado</th>
+                                       
+                                    </tr>
+                                </thead>
+                                <tbody class="text-center">
+                                    <tr>
+                                        <td>Centro 1</td>
+                                        <td>SI</td>
+                                        <td>03-08-2018</td>
+                                        <td>NO</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Centro 2</td>
+                                        <td>SI</td>
+                                        <td>03-08-2018</td>
+                                        <td>NO</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Centro 3</td>
+                                        <td>SI</td>
+                                        <td>03-08-2018</td>
+                                        <td>NO</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Centro 4</td>
+                                        <td>SI</td>
+                                        <td>03-08-2018</td>
+                                        <td>NO</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Centro 5</td>
+                                        <td>SI</td>
+                                        <td>03-08-2018</td>
+                                        <td>NO</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Centro 6</td>
+                                        <td>SI</td>
+                                        <td>03-08-2018</td>
+                                        <td>NO</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Centro 7</td>
+                                        <td>SI</td>
+                                        <td>03-08-2018</td>
+                                        <td>NO</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Centro 8</td>
+                                        <td>SI</td>
+                                        <td>03-08-2018</td>
+                                        <td>NO</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Centro 9</td>
+                                        <td>SI</td>
+                                        <td>03-08-2018</td>
+                                        <td>NO</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Centro 10</td>
+                                        <td>SI</td>
+                                        <td>03-08-2018</td>
+                                        <td>NO</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Centro 11</td>
+                                        <td>SI</td>
+                                        <td>03-08-2018</td>
+                                        <td>NO</td>
+                                    </tr>
+                                </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+        </div> <!-- .content -->
+<?php include('footer.php'); ?>
---
+++ seguimiento_lista_2.php
@@ -0,0 +1,130 @@
+<?php include('header.php'); ?>
+<div class="breadcrumbs">
+            <div class="col-sm-4">
+                <div class="page-header float-left">
+                    <div class="page-title">
+                        <h1>INABIF</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-8">
+                <div class="page-header float-right">
+                    <div class="page-title">
+                        <ol class="breadcrumb text-right">
+                            <li class="active"></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="content mt-3">
+        <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong>Lista de Seguimiento</strong>
+                            <h6>Período Noviembre 2018</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="row form-group">
+                                <div class="col-md-1 col-sm-6"><label for="select" class=" form-control-label">Grupo:</label></div>
+                                <div class="col-md-3  col-sm-6">
+                                    <select name="select" id="select" class="form-control">
+                                        <option value="1">Grupo 3</option>
+                                       
+                                    </select>
+                                </div>
+                                <div class="col-md-1 col-sm-6"><label for="select" class=" form-control-label">Residente:</label></div>
+                                <div class="col-md-4  col-sm-6">
+                                <input type="text" value="" class="form-control" placeholder="Apellido Cod">
+                                </div>
+                            </div>
+
+                            <div class="table-responsive">
+                                <table class="table">
+                                <thead class="thead-dark text-center">
+                                    <tr>
+                                    
+                                        <th scope="col">Usuarios</th>
+                                        <th scope="col">Encangado</th>
+                                        <th scope="col">Completo</th>
+                                        <th scope="col">Última actualización</th>
+                                       
+                                    </tr>
+                                </thead>
+                                <tbody class="text-center">
+                                    <tr>
+                                        <td>Grupo 1</td>
+                                        <td>Pepe</td>
+                                        <td>SI</td>
+                                        <td>03-03-2018</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Grupo 2</td>
+                                        <td>Pepe</td>
+                                        <td>SI</td>
+                                        <td>03-03-2018</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Grupo 3</td>
+                                        <td>Pepe</td>
+                                        <td>SI</td>
+                                        <td>03-03-2018</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Grupo 4</td>
+                                        <td>Pepe</td>
+                                        <td>SI</td>
+                                        <td>03-03-2018</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Grupo 5</td>
+                                        <td>Pepe</td>
+                                        <td>SI</td>
+                                        <td>03-03-2018</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Grupo 6</td>
+                                        <td>Pepe</td>
+                                        <td>SI</td>
+                                        <td>03-03-2018</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Grupo 7</td>
+                                        <td>Pepe</td>
+                                        <td>SI</td>
+                                        <td>03-03-2018</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Grupo 8</td>
+                                        <td>Pepe</td>
+                                        <td>SI</td>
+                                        <td>03-03-2018</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Grupo 9</td>
+                                        <td>Pepe</td>
+                                        <td>SI</td>
+                                        <td>03-03-2018</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Grupo 10</td>
+                                        <td>Pepe</td>
+                                        <td>SI</td>
+                                        <td>03-03-2018</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Grupo 11</td>
+                                        <td>Pepe</td>
+                                        <td>SI</td>
+                                        <td>03-03-2018</td>
+                                    </tr>
+                                </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+        </div> <!-- .content -->
+<?php include('footer.php'); ?>
